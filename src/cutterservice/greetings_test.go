b8cc7da8939e7824d0060f1b3580e5ec93ecb745
Davide Monticelli <davide.monticelli@hotmail.it>
2023-03-12 22:30:59 +0100

    aggiunto pipeline test go e test fake in go

---
+++ src/cutterservice/greetings_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "testing"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestHelloName(t *testing.T) {
+    
+}
+
+// TestHelloEmpty calls greetings.Hello with an empty string,
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+    
+}
